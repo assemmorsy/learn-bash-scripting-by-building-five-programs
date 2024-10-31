136f86bbbf2d72c2368d76160c50a746f36b5203
Assem Morsy <assemmorsy2016@gmail.com>
2024-10-31 07:14:35 +0000

    Save progress: 530.1

--- countdown.sh
+++ countdown.sh
@@ -1,8 +1,10 @@
-#! /bin/bash
+#!/bin/bash
+
 # Program that counts down to zero from a given argument
-if [[ $1 == arg1 ]] 
-then 
-  echo true 
-else 
-  echo false 
+
+if [[ $1 -lt 5 ]]
+then
+  echo true
+else
+  echo false
 fi
